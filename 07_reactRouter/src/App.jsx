96bc197b4773ead76d1e55f19034832a71444251
Nakul <nakulverma.py@gmail.com>
2025-04-25 22:30:38 +0530

    Add App and Layout components for React Router application

---
+++ 07_reactRouter/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { Header, Footer, Home, About } from '../components';
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-3xl text-center text-white font-bold'>React Router</h1>
+
+    </>
+  )
+}
+
+export default App
