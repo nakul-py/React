8815454333f43ed8861e4cf389ef5951707b6f41
Nakul <nakulverma.py@gmail.com>
2025-04-25 22:30:12 +0530

    Add Vite configuration and component exports for React application

---
+++ 07_reactRouter/components/index.js
@@ -0,0 +1,15 @@
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Home from "./Home/Home";
+import About from "./About/About";
+import Contact from "./Contact/Contact";
+import User from "./User/User";
+import Github from "./Github/Github";
+
+export { Header }
+export { Footer }
+export { Home }
+export { About }
+export { Contact }
+export { User }
+export { Github }
