9fb5a2eb509c7d0f3212dde182f9d3c63c2ca759
Nakul <nakulverma.py@gmail.com>
2025-04-27 16:01:11 +0530

    Add App component with Tailwind CSS styling and Vite logo

---
+++ 08_miniContext/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='text-blue-600 text-4xl h-40 flex items-center justify-center'
+      >Mini Context</div>
+    </>
+  )
+}
+
+export default App
