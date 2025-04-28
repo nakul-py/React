9f46d8dc57c7483bf3c3001726ede6ed75e78588
Nakul <nakulverma.py@gmail.com>
2025-04-28 19:50:08 +0530

    Wrap App component with UserContextProvider and add Login and Profile components

--- 08_miniContext/src/App.jsx
+++ 08_miniContext/src/App.jsx
@@ -1,12 +1,17 @@
 import './App.css'
+import Card from './Card'
+import Login from './components/Login'
+import Profile from './components/Profile'
+import UserContextProvider from './context/UserContextProvider'
 
 function App() {
 
   return (
-    <>
-      <div className='text-blue-600 text-4xl h-40 flex items-center justify-center'
-      >Mini Context</div>
-    </>
+    <UserContextProvider>
+      <div className='text-blue-600 text-4xl h-40 flex items-center justify-center'>Mini Context</div>
+      < Login/>
+      < Profile/>
+    </UserContextProvider>
   )
 }
 
