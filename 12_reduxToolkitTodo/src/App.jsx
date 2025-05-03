928dcef331073c76ba51151800484c4c4330f9d9
Nakul <nakulverma.py@gmail.com>
2025-05-03 16:46:45 +0530

    Enhance App component layout

--- 12_reduxToolkitTodo/src/App.jsx
+++ 12_reduxToolkitTodo/src/App.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import './App.css'
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todos'
 
 function App() {
 
   return (
     <>
-     <div>Redux Toolkit</div>
+    <div className='flex justify-center'>
+     <h1 className='text-3xl font-bold text-center mt-10 bg-gray-800 text-white py-4 rounded w-5/12  '>Todo App with 
+      <span className='text-indigo-500'> Redux</span> and 
+      <span className='text-indigo-500'> Redux Toolkit</span>
+    </h1>
+    </div>
+    <AddTodo />
+    <Todos />
     </>
   )
 }
