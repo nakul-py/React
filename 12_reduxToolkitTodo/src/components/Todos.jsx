a2379ef5d45a65927aedbae457bab8ecc5746db9
Nakul <nakulverma.py@gmail.com>
2025-05-03 16:46:23 +0530

    Add editing functionality to Todos component and update HTML structure

--- 12_reduxToolkitTodo/src/components/Todos.jsx
+++ 12_reduxToolkitTodo/src/components/Todos.jsx
@@ -1,41 +1,68 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import {removeTodo} from '../features/todo/todoSlice'
+import { removeTodo, updateTodo } from '../features/slice/todoSlice'
 
 function Todos() {
-    const todos = useSelector(state => state.todos)
-    const dispatch = useDispatch()
+  const todos = useSelector(state => state.todos)
+  const dispatch = useDispatch()
+
+  const [editableTodoId, setEditableTodoId] = React.useState(null)
+  const [editedTitle, setEditedTitle] = React.useState("")
+
+  const handleEditClick = (todo) => {
+    setEditableTodoId(todo.id)
+    setEditedTitle(todo.title)
+  }
+
+  const handleSaveClick = (todo) => {
+    dispatch(updateTodo({ id: todo.id, title: editedTitle }))
+    setEditableTodoId(null)
+    setEditedTitle("")
+  }
 
   return (
     <>
-    <div>Todos</div>
-    <ul className="list-none">
+    <div className='flex justify-center'>
+      <h1 className='text-amber-500 text-3xl font-bold mt-10 text-center bg-gray-800 py-4 rounded w-1/5'>Your Todos</h1>
+      </div>
+      <ul className="list-none">
         {todos.map((todo) => (
-          <li
-            className="mt-4 flex justify-between items-center bg-zinc-800 px-4 py-2 rounded"
-            key={todo.id}
-          >
-            <div className='text-white'>{todo.text}</div>
-            <button
-             onClick={() => dispatch(removeTodo(todo.id))}
-              className="text-white bg-red-500 border-0 py-1 px-4 focus:outline-none hover:bg-red-600 rounded text-md"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="w-6 h-6"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+          <div key={todo.id} className='flex justify-center'>
+            <li className="mt-4 flex justify-between items-center bg-zinc-800 px-4 py-2 rounded-2xl w-6/12 h-16">
+              {editableTodoId === todo.id ? (
+                <input
+                  className="text-2xl px-2 py-1 rounded"
+                  value={editedTitle}
+                  onChange={(e) => setEditedTitle(e.target.value)}
+                  autoFocus
                 />
-              </svg>
-            </button>
-          </li>
+              ) : (
+                <div className='text-white text-3xl'>{todo.title}</div>
+              )}
+              <div className='flex items-center'>
+
+              <button
+                className="w-10 h-10 rounded-lg text-sm border border-black/10 justify-center bg-gray-50 hover:bg-gray-300 shrink-0 disabled:opacity-50 ml-2"
+                onClick={() => {
+                  if (editableTodoId === todo.id) {
+                    handleSaveClick(todo)
+                  } else {
+                    handleEditClick(todo)
+                  }
+                }}
+              >
+                {editableTodoId === todo.id ? "📁" : "✏️"}
+              </button>
+
+              <button
+                onClick={() => dispatch(removeTodo(todo.id))}
+                className="ml-2 text-white bg-red-500 border-0 py-1 px-4 focus:outline-none hover:bg-red-900 rounded text-md w-10 h-10 flex justify-center items-center"
+              >
+                🗑️
+              </button>
+              </div>
+            </li>
+          </div>
         ))}
       </ul>
     </>
