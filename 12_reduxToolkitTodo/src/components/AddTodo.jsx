77a2bd37a9c0e58cbd1ea5f9bc2af6e65cbe69be
Nakul <nakulverma.py@gmail.com>
2025-05-03 16:47:58 +0530

    Add validation and improve styling in AddTodo component

--- 12_reduxToolkitTodo/src/components/AddTodo.jsx
+++ 12_reduxToolkitTodo/src/components/AddTodo.jsx
@@ -8,22 +8,30 @@ function AddTodo() {
 
     const addTodoHandler = (e) => {
         e.preventDefault()
-        if (input === "") return
-        dispatch(addTodo({ title: input })) // dispatch the addTodo action with the input value
+        if (!input){
+            alert("enter valid todo")
+            return}
+            dispatch(addTodo( newTodo )) // dispatch the addTodo action with the input value
         setInput('')
     }
+    const newTodo = {
+        title: input,
+        
+      }; 
+
+    
     return (
-        <form onSubmit={addTodoHandler} className="space-x-3 mt-12">
+        <form onSubmit={addTodoHandler} className="space-x-3 mt-12 flex justify-center">
       <input
         type="text"
-        className="bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+        className="bg-gray-800 text-center rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
         placeholder="Enter a Todo..."
         value={input}
         onChange={(e) => setInput(e.target.value)}
       />
       <button
         type="submit"
-        className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
+        className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-900 rounded text-lg"
       >
         Add Todo
       </button>
