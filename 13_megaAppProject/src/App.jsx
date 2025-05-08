da3e057310d201488b8b5dcfd143e562176cbb25
Nakul <nakulverma.py@gmail.com>
2025-05-08 17:24:19 +0530

    Add main application structure with App component

---
+++ 13_megaAppProject/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <p className='flex flex-col items-center text-9xl text-white font-bold justify-center min-h-screen p-4 bg-gray-600'>
+        Appwrite
+      </p>
+    </>
+  )
+}
+
+export default App
