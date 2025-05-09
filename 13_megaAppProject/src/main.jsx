991d9229f6a731143c0495d11258d156d5b3d056
Nakul <nakulverma.py@gmail.com>
2025-05-09 11:15:26 +0530

    Wrap App component with Redux Provider

--- 13_megaAppProject/src/main.jsx
+++ 13_megaAppProject/src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { Provider } from 'react-redux'
+import { store } from '../src/store/store.js'
+import { StrictMode } from 'react'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+  <Provider store={store}>
     <App />
-  </StrictMode>,
+  </Provider>
+  </StrictMode>
 )
