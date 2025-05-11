bf3b21491409a6dd338ee8506f92fefc00553662
Nakul <nakulverma.py@gmail.com>
2025-05-11 21:34:29 +0530

    Add Postcard component for displaying post previews with links

---
+++ 13_megaAppProject/src/components/Postcard.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import  service from '../appwrite/awconfig.js'
+import {Link} from 'react-router-dom'
+
+function Postcard({$id, title, featuredImage}) {
+    return (
+        <Link to={`/post/${$id}`}>
+            <div className='w-full bg-gray-400 px-4 rounded-xl'>
+                <div className='w-full justify-center mb-4'></div>
+                <img src={service.getFilePreview(featuredImage)} alt={title} />
+            </div>
+            <h2 className=' font-bold text-xl'>{title}</h2>
+        </Link>
+    )
+}
+
+export default React.forwardRef(Postcard)
