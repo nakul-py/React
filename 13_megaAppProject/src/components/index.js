8dd1dbc211a1149536d86564141c06232febbee6
Nakul <nakulverma.py@gmail.com>
2025-05-11 21:34:09 +0530

    Add Login, Postcard, Selector, Button, Input, and SignUp components to index export

--- 13_megaAppProject/src/components/index.js
+++ 13_megaAppProject/src/components/index.js
@@ -3,5 +3,24 @@ import Footer from "./Footer/Footer"
 import Logo from "./Logo";
 import Container from "./container/Container";
 import LogoutBtn from "./Header/LogoutBtn";
+import Login from "./Login";
+import Postcard from "./Postcard";
+import Selector from "./Selector";
+import Button from "./Button"
+import Input from "./Input"
+import SignUp from "./SignUp";
 
-export {Header, Footer, Logo, Container, LogoutBtn};
+
+export {
+       Header,
+       Footer, 
+       Logo, 
+       Container, 
+       LogoutBtn, 
+       Login, 
+       Postcard, 
+       Selector, 
+       Button, 
+       Input,
+       SignUp
+    };
