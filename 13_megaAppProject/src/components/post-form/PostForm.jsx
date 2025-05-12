76f1fc02b36d8b8de87aa093c29d03c152f259e4
Nakul <nakulverma.py@gmail.com>
2025-05-12 21:43:42 +0530

    Add PostForm component for creating and updating posts with image upload

---
+++ 13_megaAppProject/src/components/post-form/PostForm.jsx
@@ -0,0 +1,142 @@
+import React, { useCallback, useEffect } from "react";
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { Button, Input, Selector, RTE } from "../index";
+import service from "../../appwrite/awconfig";
+
+function PostForm({ post }) {
+  const navigate = useNavigate();
+  const { register, handleSubmit, watch, setValue, control, getValues } =
+    useForm({
+      defaultValues: {
+        title: post?.title || "",
+        id: post?.id || "",
+        content: post?.content || "",
+        status: post?.status || "active",
+      },
+    });
+
+  const userData = useSelector((state) => state.auth.userData);
+
+  const submit = async (data) => {
+    if (post) {
+      const file = data.image[0]
+        ? await service.uploadFile(data.image[0])
+        : null;
+
+      if (file) {
+        service.deleteFile(post.featuredImage);
+      }
+
+      const postDb = await service.updatePost(post.$id, {
+        ...data,
+        featuredImage: file ? file.$id : undefined,
+      });
+
+      if (postDb) {
+        navigate(`/post/${postDb.$id}`);
+      }
+    } else {
+      const file = data.image[0]
+        ? await service.uploadFile(data.image[0])
+        : null;
+
+      if (file) {
+        const fileId = file.$id;
+        data.featuredImage = fileId;
+        const postDb = await service.createPost({
+          ...data,
+          userId: userData.$id,
+        });
+
+        if (postDb) {
+          navigate(`/post/${postDb.$id}`);
+        }
+      }
+    }
+  };
+
+  const idTransform = useCallback((value) => {
+    if (value && typeof value === "string")
+      return value
+        .trim()
+        .toLowerCase()
+        .replace(/[^a-zA-Z\d\s]+/g, "-")
+        .replace(/\s/g, "-");
+
+    return "";
+  }, []);
+
+  useEffect(() => {
+    const subscription = watch((value, { name }) => {
+      if (name === "title") {
+        setValue("id", idTransform(value.title), { shouldValidate: true });
+      }
+      return () => subscription.unsubscribe();
+    });
+  }, [watch, idTransform, setValue]);
+
+  return (
+    <form onSubmit={handleSubmit(submit)} className="flex flex-wrap">
+      <div className="w-2/3 px-2">
+        <Input
+          label="Title :"
+          placeholder="Title"
+          className="mb-4"
+          {...register("title", { required: true })}
+        />
+        <Input
+          label="Id :"
+          placeholder="Id"
+          className="mb-4"
+          {...register("id", { required: true })}
+          onInput={(e) => {
+            setValue("id", idTransform(e.currentTarget.value), {
+              shouldValidate: true,
+            });
+          }}
+        />
+        <RTE
+          label="Content :"
+          name="content"
+          control={control}
+          defaultValue={getValues("content")}
+        />
+      </div>
+      <div className="w-1/3 px-2">
+        <Input
+          label="Featured Image :"
+          type="file"
+          className="mb-4"
+          accept="image/png, image/jpg, image/jpeg, image/gif"
+          {...register("image", { required: !post })}
+        />
+        {post && (
+          <div className="w-full mb-4">
+            <img
+              src={service.getFilePreview(post.featuredImage)}
+              alt={post.title}
+              className="rounded-lg"
+            />
+          </div>
+        )}
+        <Selector
+          options={["active", "inactive"]}
+          label="Status"
+          className="mb-4"
+          {...register("status", { required: true })}
+        />
+        <Button
+          type="submit"
+          bgColor={post ? "bg-green-500" : undefined}
+          className="w-full"
+        >
+          {post ? "Update" : "Submit"}
+        </Button>
+      </div>
+    </form>
+  );
+}
+
+export default PostForm;
