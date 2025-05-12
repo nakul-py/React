08d774a7dc4cb9da8b4c127a99fdc93c5c202a49
Nakul <nakulverma.py@gmail.com>
2025-05-12 12:45:47 +0530

    format code

--- 13_megaAppProject/src/components/AuthLayout.jsx
+++ 13_megaAppProject/src/components/AuthLayout.jsx
@@ -1,24 +1,23 @@
-import React, {useEffect, useState} from 'react'
-import { useSelector } from 'react-redux'
-import { useNavigate } from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
-export default function ProtectedContainer({children, authentication = true}) {
+export default function ProtectedContainer({
+  children,
+  authentication = true,
+}) {
+  const navigate = useNavigate();
+  const [loader, setLoader] = useState(true);
+  const authStaus = useSelector((state) => state.auth.status);
 
-    const navigate = useNavigate()
-    const [loader, setLoader] = useState(true)
-    const authStaus = useSelector(state => state.auth.status)
+  useEffect(() => {
+    if (authentication && authStaus !== authentication) {
+      navigate("/login");
+    } else if (!authentication && authStaus !== authentication) {
+      navigate("/");
+    }
+    setLoader(false);
+  }, [authStaus, navigate, authentication]);
 
-    useEffect(() => {
-        if(authentication && authStaus !== authentication) {
-            navigate("/login")
-        } else if(!authentication && authStaus !== authentication){
-            navigate("/")
-        }
-        setLoader(false)
-    }, [authStaus, navigate, authentication])
-
-    return (
-    loader ? <h1>Loading...</h1> : <>{children}</>
-    )
+  return loader ? <h1>Loading...</h1> : <>{children}</>;
 }
-
