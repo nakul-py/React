e820d11ac7371beed62db117a99d7f7e095eeff7
Nakul <nakulverma.py@gmail.com>
2025-05-14 22:50:20 +0530

    debugind

--- 13_megaAppProject/src/components/SignUp.jsx
+++ 13_megaAppProject/src/components/SignUp.jsx
@@ -43,7 +43,7 @@ function SignUp() {
         <p className="mt-2 text-center text-base text-black/60">
           Already have an account?&nbsp;
           <Link
-            to="login"
+            to="/login"
             className="font-medium text-primary transition-all duration-200 hover:underline"
           >
             Sign In
