beb2fba6445761c9f77b8f3489f0accc4c1edaf3
Nakul <nakulverma.py@gmail.com>
2025-05-14 22:49:36 +0530

    debuging

--- 13_megaAppProject/src/appwrite/auth.js
+++ 13_megaAppProject/src/appwrite/auth.js
@@ -3,53 +3,59 @@ import config from "../config/config";
 import { Client, Account, ID } from "appwrite";
 
 export class AuthService {
-    client = new Client()
-    account;
+  client = new Client();
+  account;
 
-    constructor() {
-        this.client
-        .setEndpoint(config.appwriteUrl)
-        .setProject(config.appwriteProjectId);
-        this.account = new Account(this.client)
-    }
+  constructor() {
+    this.client
+      .setEndpoint(config.appwriteUrl)
+      .setProject(config.appwriteProjectId);
+    this.account = new Account(this.client);
+  }
 
-    async createAccount({email, name, password}){
-        
-        try {
-            const userAccount = await this.account.create(ID.unique(),email, password, name);
-            if (userAccount) {
-                // call another method
-            } else {
-                return userAccount;
-            }
-        } catch (error) {
-            throw error;
-        }
+  async createAccount({ email, name, password }) {
+    try {
+      const userAccount = await this.account.create(
+        ID.unique(),
+        email,
+        password,
+        name
+      );
+      if (userAccount) {
+        // call another method
+        return this.login({email, password});
+      } else {
+        return userAccount;
+      }
+    } catch (error) {
+      throw error;
     }
-    async login({email,password}){
-        try{
-            return await this.account.createEmailPasswordSession(email , password)
-        } catch (error) {
-            throw error;
-        }
+  }
+  async login({ email, password }) {
+    try {
+      return await this.account.createEmailPasswordSession(email, password);
+    } catch (error) {
+      throw error;
     }
-    async getCurrentUser({email, password, name}){
-        try {
-            return await this.account.get(name,email,password)
-        } catch (error) {
-            throw error
-        }
-    }
-    async logout(){
-        try {
-            return await this.account.deleteSessions()
-        } catch (error) {
-            throw error
-        }
-    }
+  }
+  async getCurrentUser() {
+    try {
+      return await this.account.get();
+  } catch (error) {
+      console.log("Appwrite serive :: getCurrentUser :: error", error);
+  }
 
+  return null;
 }
-    
-const authService = new AuthService()
-export default authService
 
+  async logout() {
+    try {
+      await this.account.deleteSessions();
+    } catch (error) {
+      console.log("Appwrite serive :: logout :: error", error);
+    }
+  }
+}
+
+const authService = new AuthService();
+export default authService;
--- 13_megaAppProject/src/components/Button.jsx
+++ 13_megaAppProject/src/components/Button.jsx
@@ -1,18 +1,22 @@
-import React from 'react'
+import React from "react";
 
 function Button({
-    text,
-    type = 'button',
-    bgColor = 'bg-blue-600',
-    textColor = 'white',
-    className= '',
-    ...props
+  children,
+  type = "button",
+  bgColor = "bg-blue-600",
+  textColor = "text-white",
+  className = "",
+  ...props
 }) {
-    return (
-        <button className={` px-4 py-2 rounded-lg ${type} ${bgColor} ${textColor} ${className}`} {...props}>
-            {text}
-        </button>
-    )
+  return (
+    <button
+      type={type}
+      className={` px-4 py-2 rounded-lg ${bgColor} ${textColor} ${className}`}
+      {...props}
+    >
+      {children}
+    </button>
+  );
 }
 
-export default Button
+export default Button;
