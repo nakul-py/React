fc9cc2812c215016085cb96433ac2a8925f6d4cc
Nakul <nakulverma.py@gmail.com>
2025-05-14 22:50:06 +0530

    debugind

--- 13_megaAppProject/src/components/Selector.jsx
+++ 13_megaAppProject/src/components/Selector.jsx
@@ -10,17 +10,18 @@ function Selector({
     return (
         <div className='w-full'>
             {label && <label htmlFor={id} 
-            className='inline-block mb-1 pl-1 px-2 py-4'></label>
+            className='inline-block mb-1 pl-1 px-2 py-4'>{label}</label>
             }
             <select {...props}
             id = {id}
             ref={ref}
             className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-400 w-full ${className}`}>
-                {options?.map((option) => {
+                {options?.map((option) => (
                     <option key={option} value={option}>
                         {option}
                     </option>
-                })}
+                    )
+                )}
 
             </select>
         </div>
