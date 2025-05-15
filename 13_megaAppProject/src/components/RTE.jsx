7461b2a03cef47ea6a40f59822e5eb1c8a62fa8e
Nakul <nakulverma.py@gmail.com>
2025-05-15 21:31:24 +0530

    ..

--- 13_megaAppProject/src/components/RTE.jsx
+++ 13_megaAppProject/src/components/RTE.jsx
@@ -12,6 +12,7 @@ export default function RTE({ name, control, label, defaultValue = "" }) {
         control={control}
         render={({ field: { onChange } }) => (
           <Editor
+          apiKey="emv1ppjzqzq92r3r0fqqb47kr6ad4mrbm5tzphj9hrg4dls4"
             initialValue={defaultValue}
             init={{
               initialValue: defaultValue,
