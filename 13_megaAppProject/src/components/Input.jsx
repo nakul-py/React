887ef76bd4a8c3b03f33ddbee53713b654b3e213
Nakul <nakulverma.py@gmail.com>
2025-05-16 21:43:50 +0530

    Refactor Input component for improved readability and structure

--- 13_megaAppProject/src/components/Input.jsx
+++ 13_megaAppProject/src/components/Input.jsx
@@ -1,30 +1,34 @@
-import React, {useId} from 'react'
+import React, { useId } from "react";
 
-const Input = React.forwardRef( function Input({
-    label,
-    type = 'text',
-    className = "",
-    ...props
-}, ref){
-    const id = useId()
-        return (
-            <div className='w-full'> {label && <label
-                className='inline-block mb-1 pl-1 px-2 py-4'
-                htmlFor='id'> {label} </label>
-                } 
-                <input type={type} 
-                className={`px-3 py-2 rounded-lg
+const Input = React.forwardRef(function Input(
+  { label, type = "text", className = "", children, ...props },
+  ref
+) {
+  const id = useId();
+
+  return (
+    <div className="w-full">
+      {" "}
+      {label && (
+        <label className="inline-block mb-1 pl-1 px-2 py-4" htmlFor="id">
+          {" "}
+          {label}{" "}
+        </label>
+      )}
+      <input
+        type={type}
+        className={`px-3 py-2 rounded-lg
                bg-white text-black outline-none
                focus:bg-gray-600 duration-100 border 
                border-gray-200 w-full 
-                ${className}`} 
-                ref={ref}
-                {...props}
-                id={id}
-                />
-            </div>
-        )
-})
+                ${className}`}
+        ref={ref}
+        {...props}
+        id={id}
+      />
+      <div className="absolute top-1/1 -translate-y-1/1 text-xl cursor-pointer">{children}</div>
+    </div>
+  );
+});
 
-
-export default Input
+export default Input;
