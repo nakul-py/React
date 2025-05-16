8e3ce467997ff526fc162431f74ee2c78a31f2a8
Nakul <nakulverma.py@gmail.com>
2025-05-16 21:44:08 +0530

    Update Postcard component styles and improve image alt attribute

--- 13_megaAppProject/src/components/Postcard.jsx
+++ 13_megaAppProject/src/components/Postcard.jsx
@@ -4,16 +4,14 @@ import { Link } from "react-router-dom";
 
 function Postcard({ $id, title, featuredImage }) {
   const imageUrl = featuredImage? service.getFilePreview(featuredImage) : null
-  console.log("Image URL:", imageUrl);
-
 
   return (
     <Link to={`/post/${$id}`}>
-      <div className="w-full bg-gray-400 px-4 rounded-xl">
+      <div className="w-full bg-purple-400 px-4 rounded-xl">
         <div className="w-full justify-center mb-4"></div>
         <img src={imageUrl}
-         alt={title} 
-        className="rounded-xl mb-4" />
+         alt={$id} 
+        className="rounded-xl mb-10" />
       </div>
       <h2 className=" font-bold text-xl">{title}</h2>
     </Link>
