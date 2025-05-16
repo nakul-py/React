adc188804309fd9908686975fa826d6504d717d1
Nakul <nakulverma.py@gmail.com>
2025-05-16 21:44:33 +0530

    Update hover color of Logout button in Header component

--- 13_megaAppProject/src/components/Header/LogoutBtn.jsx
+++ 13_megaAppProject/src/components/Header/LogoutBtn.jsx
@@ -13,7 +13,7 @@ function LogoutBtn() {
   };
   return (
     <button
-      className="inline-block px-6 py-2 duration-200 hover:bg-blue-400 rounded-full"
+      className="inline-block px-6 py-2 duration-200 hover:bg-red-400 rounded-full"
       onClick={logoutHandeler}
     >
       Logout
