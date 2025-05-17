9d9b02e34a4d54624458664fb1ba8770975622d5
Nakul <nakulverma.py@gmail.com>
2025-05-17 14:26:19 +0530

    Refactor Home component to use Redux for state management, add loading state during post fetching

--- 13_megaAppProject/src/pages/Home.jsx
+++ 13_megaAppProject/src/pages/Home.jsx
@@ -1,29 +1,59 @@
 import React, { useEffect, useState } from "react";
 import service from "../appwrite/awconfig";
 import { Container, Postcard } from "../components";
+import { setPosts } from "../store/postSlice";
+import { useDispatch, useSelector } from "react-redux";
 
 function Home() {
-  const [posts, setPosts] = useState([]);
+  const dispatch = useDispatch();
+  const posts = useSelector((state) => state.post.posts || []);
+  // console.log("Current Redux Posts:", posts);
+
+  const authStatus = useSelector((state) => state.auth.status);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    service.getPostss().then((posts) => {
-      if (posts) {
-        setPosts(posts.documents);
+    const fetchPosts = async () => {
+      try {
+        const allPosts = await service.getPostss();
+        dispatch(setPosts(allPosts.documents));
+      } catch (error) {
+        console.error("Failed to fetch posts", error);
+      } finally {
+        setLoading(false);
       }
-    });
-  }, []);
+    };
+
+    fetchPosts();
+  }, [dispatch]);
+
+  if (loading) {
+    return (
+      <div className="w-full py-8 mt-4 text-center">
+        <Container>
+          <p className="text-xl">Loading Posts...</p>
+        </Container>
+      </div>
+    );
+  }
+
+  if (!authStatus) {
+    return (
+      <div className="w-full py-8 mt-4 text-center">
+        <Container>
+          <h1 className="text-2xl font-bold text-gray-600">
+            Login to read posts
+          </h1>
+        </Container>
+      </div>
+    );
+  }
 
   if (posts.length === 0) {
     return (
       <div className="w-full py-8 mt-4 text-center">
         <Container>
-          <div className="flex flex-wrap">
-            <div className="p-2 w-full">
-              <h1 className="text-2xl font-bold hover:text-gray-500">
-                Login to read posts
-              </h1>
-            </div>
-          </div>
+          <h1 className="text-xl text-gray-500">No posts available</h1>
         </Container>
       </div>
     );
@@ -33,7 +63,10 @@ function Home() {
       <Container>
         <div className="flex flex-wrap">
           {posts.map((post) => (
-            <div key={post.$id} className="p-2 w-1/4">
+            <div
+              key={post.$id}
+              className="p-2 w-full sm:w-1/2 md:w-1/3 lg:w-1/4"
+            >
               <Postcard {...post} />
             </div>
           ))}
