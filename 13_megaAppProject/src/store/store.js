417443461da9e5e68b968aa55287f249f783c25a
Nakul <nakulverma.py@gmail.com>
2025-05-17 14:26:32 +0530

    Add postSlice to Redux store for improved state management of posts

--- 13_megaAppProject/src/store/store.js
+++ 13_megaAppProject/src/store/store.js
@@ -1,12 +1,12 @@
-import {configureStore} from '@reduxjs/toolkit'
-import authSlice from './authSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import authSlice from "./authSlice";
+import postSlice from "./postSlice";
 
 const store = configureStore({
-    reducer: {
-        auth : authSlice,
-        //TODO: add more slices here for posts
-    }
+  reducer: {
+    auth: authSlice,
+    post: postSlice,
+  },
 });
-
 
 export default store;
