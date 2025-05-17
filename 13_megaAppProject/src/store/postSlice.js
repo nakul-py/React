e00cd654e50a0fc7653440ab380db1fc57562f91
Nakul <nakulverma.py@gmail.com>
2025-05-17 14:26:25 +0530

    Refactor postSlice to improve state management by renaming reducers and adding setPosts and addPost actions

--- 13_megaAppProject/src/store/postSlice.js
+++ 13_megaAppProject/src/store/postSlice.js
@@ -1,31 +1,36 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    posts: [],
-    uploadedFile: [],
-    loading: false,
-    error: null
-}
+  posts: [],
+  uploadedFile: [],
+  loading: false,
+  error: null,
+};
 
 export const postSlice = createSlice({
-    name: "post",
-    initialState,
-    reducers: {
-        createPost:(state, action) => {
-            state.posts = action.payload;
-        },
-        deletePost:(state, action) => {
-            state.posts = state.posts.filter(post => post.$id !== action.payload)
-        },
-        uploadFile:(state, action) => {
-            state.uploadedFile.push(action.payload)
-        },
-        deleteFile:(state, action) => {
-            state.uploadedFile = state.uploadedFile.filter(file => file.$id !== action.payload)
-        },
-        
-    }
-})
+  name: "post",
+  initialState,
+  reducers: {
+    setPosts: (state, action) => {
+      state.posts = action.payload;
+    },
+    addPost: (state, action) => {
+      state.posts.push(action.payload);
+    },
+    deletePost: (state, action) => {
+      state.posts = state.posts.filter((post) => post.$id !== action.payload);
+    },
+    uploadFile: (state, action) => {
+      state.uploadedFile.push(action.payload);
+    },
+    deleteFile: (state, action) => {
+      state.uploadedFile = state.uploadedFile.filter(
+        (file) => file.$id !== action.payload
+      );
+    },
+  },
+});
 
-export const {createPost, deleteFile, deletePost, uploadFile} = postSlice.actions
-export default postSlice.reducer
+export const { setPosts, addPost, deleteFile, deletePost, uploadFile } =
+  postSlice.actions;
+export default postSlice.reducer;
